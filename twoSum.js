de7b263990a4f8fe9d489ff55ff5828e870beeff
Harpreet Singh <HarpreetSingh.Duggal@iress.com>
2020-12-07 20:18:34 +1100

    twoSum leetcode

---
+++ twoSum.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//complexity O(n) and space O(n)
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const hashMap = new Map();
+    for (let i = nums.length-1; i >= 0; --i) {
+        const secondPair = target - nums[i];
+        if (hashMap.has(secondPair)) {
+            return [i, hashMap.get(secondPair)];
+        }
+        hashMap.set(nums[i], i);
+    }
+};
